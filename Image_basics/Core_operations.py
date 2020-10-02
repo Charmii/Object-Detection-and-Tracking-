7ec34867242272176cfac741643d9092f92ab214
Charmi Shah <charmishah1030@gmail.com>
2020-10-02 17:37:22 +0530

    Update Core_operations.py

--- Image_basics/Core_operations.py
+++ Image_basics/Core_operations.py
@@ -8,21 +8,38 @@ img2 = cv2.imread('img2.jpg')
 drawing = False # true if mouse is pressed
 mode = True # if True, draw rectangle. Press 'm' to toggle to curve
 ix,iy = -1,-1
-def draw_circle_resctangle(event,x,y,flags,params):
-    global drawing,mode,ix,iy
+drawing = False # true if mouse is pressed
+mode = True # if True, draw rectangle. Press 'm' to toggle to curve
+ix,iy = -1,-1
+def draw_circle(event,x,y,flags,param):
+    global ix,iy,drawing,mode
+
     if event == cv2.EVENT_LBUTTONDOWN:
         drawing = True
         ix,iy = x,y
-  
+
     elif event == cv2.EVENT_MOUSEMOVE:
         if drawing == True:
             if mode == True:
-                cv2.rectangle(img, (ix,iy), (x,y) , (0,0,0) , -1)
-            elif mode == False:
-                cv2.circle(img, (x,y), 30, (250,250,250) , -1)
+                cv2.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
+            else:
+                cv2.circle(img,(x,y),5,(0,0,255),-1)
+
     elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
         if mode == True:
-                cv2.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
+            cv2.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
         else:
-            cv2.circle(img,(x,y),30,(0,0,255),-1)
+            cv2.circle(img,(x,y),5,(0,0,255),-1)
+            
+cv2.namedWindow('image')
+cv2.setMouseCallback('image',draw_circle_rectangle)
+while(1):
+    cv2.imshow('image',img)
+
+    k = cv2.waitKey(1) & 0xFF
+    if k == ord('m'):
+        mode = not mode
+    elif k == 27:
+        break
+cv2.destroyAllWindows()
